9e37a39ea2e9970bcdca2599b70f8da518957d5f
Nischay Joshi <nischayjoshi2312@gmail.com>
2023-04-05 21:50:01 -0700

    Started adding functionality to manually control Arm Joints

--- src/3AxisRoboticArm/3AxisRoboticArm.ino
+++ src/3AxisRoboticArm/3AxisRoboticArm.ino
@@ -11,44 +11,50 @@
 //Create the base servo Object
 ServoLib BaseServo(BASE_SERVO_PIN, BASE_SERVO_MAX, BASE_SERVO_MIN, BASE_SERVO_SPEED);
 
-//Function Declarations
-void GreetUser();
-
 void setup() {
   Serial.begin(9600);
   BaseServo.SetupServo();
   GreetUser();
+  pinMode(POT_PIN, INPUT);
 }
 
 int BasePos = 0;
+bool ReadPot = 0;
  
 void loop() {
-  //Read Serial Data, The format is as follows:
-  //Serial Data Format: <BasePos>, <ShoulderPos>, <ElbowPos>
-  //Example: 90, 90, 90
+
   while(Serial.available() > 0){
     String SerialData = Serial.readStringUntil('\n');
+    //if string is exactly like this: "flip", then the base arm will take in potentiometer input
+    if(SerialData == "flip"){
+        ReadPot = !ReadPot;
+        if(ReadPot){
+          BaseServo.UpdateServoParams(BASE_SERVO_MAX, BASE_SERVO_MIN, 5);
+        }
+        else{
+          BaseServo.UpdateServoParams(BASE_SERVO_MAX, BASE_SERVO_MIN, BASE_SERVO_SPEED);
+        }
+      }
     Serial.print("Serial Data Echoed: ");
     Serial.println(SerialData);
-    BasePos = SerialData.substring(0, SerialData.indexOf(',')).toInt();
-    SerialData = SerialData.substring(SerialData.indexOf(',') + 1);
-    int ShoulderPos = SerialData.substring(0, SerialData.indexOf(',')).toInt();
-    SerialData = SerialData.substring(SerialData.indexOf(',') + 1);
-    int ElbowPos = SerialData.toInt();
-    Serial.println("BasePos: " + String(BasePos) + " ShoulderPos: " + String(ShoulderPos) + " ElbowPos: " + String(ElbowPos));  
+    if(!ReadPot){
+      BasePos = SerialData.substring(0, SerialData.indexOf(',')).toInt();
+      SerialData = SerialData.substring(SerialData.indexOf(',') + 1);
+      int ShoulderPos = SerialData.substring(0, SerialData.indexOf(',')).toInt();
+      SerialData = SerialData.substring(SerialData.indexOf(',') + 1);
+      int ElbowPos = SerialData.toInt();
+      Serial.println("BasePos: " + String(BasePos) + " ShoulderPos: " + String(ShoulderPos) + " ElbowPos: " + String(ElbowPos));  
+      BaseServo.ServoGoto(BasePos);
+    }
+  } 
+  if(ReadPot){
+    int value = analogRead(POT_PIN);
+    BasePos = map(value, 250, 1023, BASE_SERVO_MIN, BASE_SERVO_MAX);
     BaseServo.ServoGoto(BasePos);
-  }  
+    // Serial.print("Potentiometer Value: ");
+    // Serial.println(value);
+    // Serial.println("BasePos: " + String(BasePos));
+    // delay(500);  
+  }
   delay(LOOP_DELAY);
-}
-
-void GreetUser(){
-  //Greet the user
-  Serial.print("Welcome to ");
-  Serial.println(SYSTEM_NAME);
-  Serial.print("Version: ");
-  Serial.println(SYSTEM_VERSION);
-  Serial.print("Author: ");
-  Serial.println(SYSTEM_AUTHOR);
-  Serial.println("Serial Data Format: <BasePos>, <ShoulderPos>, <ElbowPos>");
-  Serial.println("--------------------------------------------");
 }
--- src/3AxisRoboticArm/Constants.h
+++ src/3AxisRoboticArm/Constants.h
@@ -8,6 +8,8 @@
 #ifndef CONSTANTS_H_
 #define CONSTANTS_H_
 
+#include <Arduino.h>
+
 // System Constants
 #define SYSTEM_VERSION "1.0.0"
 #define SYSTEM_NAME "3AxisRoboticArm"
@@ -15,11 +17,19 @@
 #define LOOP_FREQUENCY 50 
 #define LOOP_DELAY 1000/LOOP_FREQUENCY
 
+// Constants for Analog input pins
+#define POT_PIN A0
+#define POT_MIN 0
+#define POT_MAX 1023
+
 
 // Base Servo Constants
 #define BASE_SERVO_PIN 9
 #define BASE_SERVO_MIN 0
 #define BASE_SERVO_MAX 180
 #define BASE_SERVO_SPEED 4
+
+// Function Declarations
+void GreetUser();
 
 #endif

80345141c73e49d276e7c087fd48fdd19cc61398
Nischay Joshi <nischayjoshi2312@gmail.com>
2023-04-09 12:18:35 -0700

    Begin working on Robot Eyes Animation

---
+++ src/LCDEyes/LCDEyes.ino
@@ -0,0 +1,23 @@
+//           frames++;
+//       if(millis() - ttime > 5000){
+//         fps = frames*1.0/5.0;  
+//         ttime = millis();          
+//         frames = 0;
+//       }
+
+// float frames = 0;
+// float fps = 0;
+// unsigned long ttime = 0;
+
+// Load TFT driver library
+#include <SPI.h>
+#include <TFT_eSPI.h>
+TFT_eSPI tft;           // A single instance is used for 1 or 2 displays
+
+void setup(){
+
+}
+
+void loop(){
+  
+}
